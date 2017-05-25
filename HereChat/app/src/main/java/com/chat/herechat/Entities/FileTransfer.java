8a0c26234c46f276e1a008b2f37bcb62b4ee8942
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-05-25 19:56:28 +0100

    HereChat code reorganized

--- HereChat/app/src/main/java/com/chat/herechat/Entities/FileTransfer.java
+++
@@ -1,8 +0,0 @@
-package com.chat.herechat.Entities;
-
-/**
- * Created by Manuel Gomes on 11/05/2017.
- */
-
-public class FileTransfer {
-}
--- HereChat/app/src/main/java/com/chat/herechat/Entities/Message.java
+++
@@ -1,4 +0,0 @@
-package com.chat.herechat.Entities;
-
-public class Message {
-}
